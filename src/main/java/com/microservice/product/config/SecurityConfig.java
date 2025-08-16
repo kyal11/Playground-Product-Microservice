c1678f920116490253218b014d83db04e9a53887
kyal11 <rizkyalarief11@gmail.com>
2025-08-17 00:19:30 +0700

    feat: add config redis,caching & docker redis

--- src/main/java/com/microservice/product/config/SecurityConfig.java
+++ src/main/java/com/microservice/product/config/SecurityConfig.java
@@ -3,12 +3,14 @@ package com.microservice.product.config;
 import com.microservice.product.security.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
