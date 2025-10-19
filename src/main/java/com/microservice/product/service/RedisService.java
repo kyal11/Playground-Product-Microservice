44e0fae7fe2c8984ff29d890c89a84d34aff091e
kyal11 <rizkyalarief11@gmail.com>
2025-10-19 20:24:33 +0700

    fix: redis config & service

--- src/main/java/com/microservice/product/service/RedisService.java
+++ src/main/java/com/microservice/product/service/RedisService.java
@@ -1,4 +1,38 @@
 package com.microservice.product.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
 public class RedisService {
+    private final RedisTemplate<String, Object> redisTemplate;
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public <T> void save(String key, T value, long ttlSeconds) {
+        if (ttlSeconds > 0) {
+            redisTemplate.opsForValue().set(key, value, ttlSeconds, TimeUnit.SECONDS);
+        } else {
+            redisTemplate.opsForValue().set(key, value);
+        }
+    }
+
+    public <T> T get(String key, Class<T> clazz) {
+        Object data = redisTemplate.opsForValue().get(key);
+        if (data == null) return null;
+
+        return objectMapper.convertValue(data, clazz);
+    }
+
+    public void delete(String key) {
+        redisTemplate.delete(key);
+    }
+
+    public boolean exists(String key) {
+        return redisTemplate.hasKey(key);
+    }
 }
